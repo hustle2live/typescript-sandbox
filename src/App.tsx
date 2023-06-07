d5982fd9d5cd661ecc1ec3bd52bd91bc06306a8e
hustle2live <kuz.v@ukr.net>
2023-06-07 12:17:14 +0300

    initial gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,73 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
-  let info: {
-    officeId: number;
-    isOpened: boolean;
-    contacts: {
-      phone: string;
-      email: string;
-      address: {
-        city: string;
+   let info: {
+      officeId: number;
+      isOpened: boolean;
+      contacts: {
+         phone: string;
+         email: string;
+         address: {
+            city: string;
+         };
       };
-    };
-  };
+   };
 
-  const user = {
-    officeId: 45,
-    isOpened: false,
-    contacts: {
-      phone: '+38090000000',
-      email: 'my@gmail.com',
-      address: {
-        city: 'NewYork'
-      }
-    }
-  };
+   const user = {
+      officeId: 45,
+      isOpened: false,
+      contacts: {
+         phone: '+38090000000',
+         email: 'my@gmail.com',
+         address: {
+            city: 'NewYork',
+         },
+      },
+   };
 
-  // info = user;
+   // info = user;
 
+   const cars: string[] = ['honda', 'bmw', 'cadillac', 'audi'];
 
+   for (const car of cars) {
+      console.log(car.toUpperCase());
+   }
 
+   // taples
 
+   const someArr: [number, string] = [101, 'ggd'];
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   const someSecond: readonly [number, string, ...boolean[]] = [
+      101,
+      'ggd',
+      true,
+      false,
+   ];
+
+   console.log(someArr);
+
+   return (
+      <div className="App">
+         <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+               Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <a
+               className="App-link"
+               href="https://reactjs.org"
+               target="_blank"
+               rel="noopener noreferrer"
+            >
+               Learn React
+            </a>
+         </header>
+      </div>
+   );
 }
 
 export default App;
